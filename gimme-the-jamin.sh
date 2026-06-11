c10edf02463e5bc186d1dde5dfd244c0dae8fcaf
MzTechnology97 <86824210+MzTechnology97@users.noreply.github.com>
2026-06-11 17:14:22 +0200

    Update gimme-the-jamin.sh

--- gimme-the-jamin.sh
+++ gimme-the-jamin.sh
@@ -15,7 +15,7 @@ install_feature() {
 install_feature better-init
 install_feature skip-setup
 install_feature moonraker
-install_feature fluidd
+#install_feature fluidd
 install_feature screws_tilt_adjust
 install_feature cartographer
 install_feature abort_homing
